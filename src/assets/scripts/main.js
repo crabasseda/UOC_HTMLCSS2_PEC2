d49aa7667e2a965a609893594d24db56f148d00e
[clara rabasseda cases] <[crabasseda@uoc.edu]>
2025-05-11 11:50:43 +0200

    main project

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,16 +1,24 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".needs-validation");
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
 
-/**
- * Write any other JavaScript below
- */
+    if (form.checkValidity()) {
+      // Si el formulario es válido, mostramos el SweetAlert
+      Swal.fire({
+        title: "¡Inscripción enviada!",
+        text: "Gracias por inscribirte. Nos vemos en las jornadas.",
+        icon: "success",
+        confirmButtonText: "¡Genial!",
+      });
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+      form.reset(); // Limpiamos el formulario
+      form.classList.remove("was-validated");
+    } else {
+      // Si el formulario no es válido, activamos validaciones de Bootstrap
+      form.classList.add("was-validated");
+    }
+  });
+});
